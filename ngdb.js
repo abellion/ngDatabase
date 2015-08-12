e73aee948b4e5a50913b1a11790c65e9058bb294
Antoine Bellion <antoinebellion@macbook-pro-de-antoine-2.home>
2015-08-12 12:16:28 +0200

    major update

--- ngdb.js
+++ ngdb.js
@@ -2,150 +2,75 @@ angular
 	.module('ngDatabase', ['ngCordova'])
 	.factory('ngdb', ngdb);
 
-ngdb.$inject = ['$q', '$cordovaSQLite', 'NGDB_CONFIG'];
+ngdb.$inject = ['$q', '$cordovaSQLite'];
 
-function ngdb($q, $cordovaSQLite, DB_CONFIG) {
+function ngdb($q, $cordovaSQLite) {
 	var self = this;
 	var _db = null;
-	var _currentTable = null;
-	var _currentBy = null;
-	var _currentOrder = null;
+	var _dbSchema = null;
+	var _ngdbUtils = new ngdbUtils();
 
 	/*
 	** PRIVATE METHODS
 	*/
-	var _isObject = function(obj) {
-		return (typeof obj === "object")
-	};
+	var _dbConnexion = function() {
+		var db = null;
 
-	var _isJson = function(json) {
-		try {
-			JSON.parse(json);
-		} catch(e) {
-			return (false);
-		}
+		if (window.cordova) {
+   			db = $cordovaSQLite.openDB('ngdb.db');
+        }
+        else {
+			db = window.openDatabase('ngdb.db', '1', 'ngdb.db', -1);
+        }
 
-		return (true);
-	};
-
-	var _followObject = function(obj, callback) {
-		var keys = Object.keys(obj);
-
-		keys.forEach(function(key) {
-			var val = obj[key];
-
-			if (val !== undefined && val !== null) {
-				callback(val, key);
-			}
-		});
-	};
-
-	var _transformData = function(data) {
-		var formated = {};
-		var table = DB_CONFIG[_currentTable];
-
-		_followObject(data, function(fieldValue, fieldName) {
-			if (table[fieldName]) {
-				if (_isObject(fieldValue)) {
-					fieldValue = JSON.stringify(fieldValue);
-				}
-				else if (_isJson(fieldValue)) {
-					fieldValue = JSON.parse(fieldValue);
-				}
-				formated[fieldName] = fieldValue;
-			}
-		});
-
-		return (formated);
-	};
-
-	var _makeQuery = function(query) {
-		var deferred = $q.defer();
-
-		self.query(query).then(function(result) {
-			deferred.resolve(result);
-		}, function(error) {
-			throw new Error(error['message'] + "\n" + query);
-			deferred.reject(error);
-		});
-
-		return (deferred.promise);
-	};
-
-	var _resetParams = function() {
-		_currentTable = null;
-		_currentBy = null;
-		_currentOrder = null;
-	};
-
-	var _constructSubParams = function(query) {
-		var subParams = [
-			{"condition": _currentBy, "value": " WHERE " + _currentBy},
-			{"condition": _currentOrder, "value": " ORDER BY " + _currentOrder}
-		];
-
-		subParams.forEach(function(val) {
-			query += (val["condition"]) ? val["value"] : "";
-		});
-
-		return (query);
-	};
-
-	/*
-	** USER SETTER METHODS
-	*/
-	self.setTable = function(table) {
-		_currentTable = table;
-
-		return (self);
-	};
-
-	self.setBy = function(by) {
-		var by_formated = [];
-
-		_followObject(by, function(val, key) {
-			by_formated.push(key + " = '" + val + "'");
-		})
-		_currentBy = by_formated.join(' and ');
-
-		return (self);
-	};
-
-	self.setOrder = function(order) {
-		var order_formated = [];
-
-		_followObject(order, function(val, key) {
-			order_formated.push(key + " " + val);
-		});
-		_currentOrder = order_formated.join(', ');
-
-		return (self);
+        return (db);
 	};
 
 	/*
 	** INIT USER METHOD
 	*/
-    self.init = function() {
-        if (window.cordova) {
-   			_db = $cordovaSQLite.openDB('ngdb.db');
-        }
-        else {
-			_db = window.openDatabase('ngdb.db', '1', 'ngdb.db', 1024 * 1024 * 100);
-        }
+    self.init = function(dbSchema) {
+    	var types = {
+    		ID: 		'integer primary key',
+    		STRING: 	'text',
+    		NUMBER: 	'integer',
+    		BOOLEAN: 	'boolean',
+    		OBJECT: 	'text',
+    		ARRAY: 		'text',
+    		DATE: 		'datetime'
+    	};
 
-        _followObject(DB_CONFIG, function(table, tableName) {
+    	_dbSchema = dbSchema;
+    	_db = _dbConnexion();
+        _ngdbUtils._followObject(_dbSchema, function(table, tableName) {
             var columns = [];
 
-            _followObject(table, function(columnType, columnName) {
-                columns.push(columnName + ' ' + columnType);
+            _ngdbUtils._followObject(table, function(columnType, columnName) {
+            	if (!types[columnType]) {
+            		_errorHandler("Unable to find '"+ columnType +"' datatype.");
+            	}
+                columns.push(columnName + ' ' + types[columnType]);
             });
             self.query('CREATE TABLE IF NOT EXISTS ' + tableName + ' (' + columns.join(', ') + ')');
         });
     };
 
 	/*
+	** USER GETTER METHODS
+	*/
+	self.getRepository = function(repositoryName) {
+		var repository = new ngdbRepository(repositoryName, self);
+
+		return (repository);
+	};
+
+	/*
 	** LOW LEVEL USER METHODS
 	*/
+	self.getDbSchema = function() {
+		return (_dbSchema);
+	};
+
 	self.query = function(query, bindings) {
         var deferred = $q.defer();
         bindings = (typeof bindings !== 'undefined') ? bindings : [];
@@ -175,21 +100,105 @@ function ngdb($q, $cordovaSQLite, DB_CONFIG) {
         return (result.rows.item(0));
     };
 
+	return (self);
+}
+
+function ngdbRepository(repository, ngdb) {
+	var self = this;
+	/* VARIABLE ATTRIBUTS */
+	var _currentRepo = repository;
+	var _currentOrder = null;
+	var _currentBy = null;
+	var _currentLimit = [];
+	/* FUNCTION ATTRIBUTS */
+	var _ngdb = ngdb;
+	var _ngdbUtils = new ngdbUtils();
+
 	/*
-	** HIGH LEVEL USER METHODS
+	** PRIVATE METHODS
+	*/
+	var _resetParams = function() {
+		_currentBy = null;
+		_currentOrder = null;
+		_currentLimit = [];
+	};
+
+	var _constructSubParams = function(query) {
+		var subParams = [
+			{"condition": _currentBy, "value": " WHERE " + _currentBy},
+			{"condition": _currentOrder, "value": " ORDER BY " + _currentOrder},
+			{"condition": _currentLimit.length, "value": " LIMIT " + _currentLimit[0] + ", " + _currentLimit[1]}
+		];
+
+		subParams.forEach(function(val) {
+			query += (val["condition"]) ? val["value"] : "";
+		});
+
+		return (query);
+	};
+
+	/*
+	** SETTERS
+	*/
+	self.setBy = function(by) {
+		var by_formated = [];
+
+		_ngdbUtils._followObject(by, function(val, key) {
+			by_formated.push(key + " = '" + val + "'");
+		})
+		_currentBy = by_formated.join(' and ');
+
+		return (self);
+	};
+
+	self.setOrder = function(order) {
+		var order_formated = [];
+
+		_ngdbUtils._followObject(order, function(val, key) {
+			order_formated.push(key + " " + val);
+		});
+		_currentOrder = order_formated.join(', ');
+
+		return (self);
+	};
+
+	self.setLimit = function(from, to) {
+		_currentLimit[0] = parseInt(from, 10);
+		_currentLimit[1] = parseInt(to, 10);
+
+		return (self);
+	};
+
+	/*
+	** DB TRANSACTIONS
 	*/
 	self.get = function() {
-		var query = "SELECT * FROM " + _currentTable;
+		var query = "SELECT * FROM " + _currentRepo;
 		query = _constructSubParams(query);
 
 		return (
-			_makeQuery(query)
+			_ngdb.query(query)
 			.then(function(result) {
-				var fetched = self.fetchAll(result);
+				var fetched = _ngdb.fetchAll(result);
 
 				fetched.forEach(function(val, index) {
-					fetched[index] = _transformData(val);
+					fetched[index] = _ngdbUtils._transformData(val, _ngdb.getDbSchema()[_currentRepo]);
 				});
+
+				return (_resetParams(), fetched);
+			})
+		);
+	};
+
+	self.getOne = function() {
+		var query = "SELECT * FROM " + _currentRepo;
+		query = _constructSubParams(query);
+
+		return (
+			_ngdb.query(query)
+			.then(function(result) {
+				var fetched = _ngdb.fetch(result);
+				fetched = _ngdbUtils._transformData(fetched, _ngdb.getDbSchema()[_currentRepo]);
 
 				return (_resetParams(), fetched);
 			})
@@ -199,36 +208,92 @@ function ngdb($q, $cordovaSQLite, DB_CONFIG) {
 	self.add = function(data) {
 		var fields = [];
 		var values = [];
-		data = _transformData(data);
+		data = _ngdbUtils._transformData(data, _ngdb.getDbSchema()[_currentRepo]);
 
-		_followObject(data, function(val, key) {
+		_ngdbUtils._followObject(data, function(val, key) {
 			fields.push(key);
 			values.push("'" + val + "'");
 		});
-		var query = "INSERT INTO " + _currentTable + "(" + fields.join(", ") + ") VALUES(" + values.join(", ") + ")";
+		var query = "INSERT INTO " + _currentRepo + "(" + fields.join(", ") + ") VALUES(" + values.join(", ") + ")";
 
-		return (_resetParams(), _makeQuery(query));
+		return (_resetParams(), _ngdb.query(query));
 	};
 
 	self.update = function(data) {
 		var data_formated = [];
-		data = _transformData(data);
+		data = _ngdbUtils._transformData(data, _ngdb.getDbSchema()[_currentRepo]);
 
-		_followObject(data, function(val, key) {
+		_ngdbUtils._followObject(data, function(val, key) {
 			data_formated.push(key + " = '" + val + "'");
 		});
-		var query = "UPDATE " + _currentTable + " SET " + data_formated.join(", ");
+		var query = "UPDATE " + _currentRepo + " SET " + data_formated.join(", ");
 		query = _constructSubParams(query);
 
-		return (_resetParams(), _makeQuery(query));
+		return (_resetParams(), _ngdb.query(query));
 	};
 
 	self.delete = function() {
-		var query= "DELETE FROM " + _currentTable;
+		var query= "DELETE FROM " + _currentRepo;
 		query = _constructSubParams(query);
 
-		return (_resetParams(), _makeQuery(query));
+		return (_resetParams(), _ngdb.query(query));
+	};
+}
+
+function ngdbUtils() {
+	var self = this;
+
+	/*
+	** PRIVATE METHODS
+	*/
+	var _isObject = function(obj) {
+		return (typeof obj === "object")
 	};
 
-	return (self);
+	var _isJson = function(json) {
+		try {
+			JSON.parse(json);
+		} catch(e) {
+			return (false);
+		}
+
+		return (true);
+	};
+
+	/*
+	** PROTECTED METHODS
+	*/
+	self._followObject = function(obj, callback) {
+		var keys = Object.keys(obj);
+
+		keys.forEach(function(key) {
+			var val = obj[key];
+
+			if (val !== undefined && val !== null) {
+				callback(val, key);
+			}
+		});
+	};
+
+	self._transformData = function(data, repoSchema) {
+		var formated = {};
+
+		self._followObject(data, function(fieldValue, fieldName) {
+			if (repoSchema[fieldName]) {
+				if (_isObject(fieldValue)) {
+					fieldValue = JSON.stringify(fieldValue);
+				}
+				else if (_isJson(fieldValue)) {
+					fieldValue = JSON.parse(fieldValue);
+				}
+				formated[fieldName] = fieldValue;
+			}
+		});
+
+		return (formated);
+	};
+
+	self._errorHandler = function(message) {
+		throw(new Error("NGDB Error : " + message, "", ""));
+	};
 }
